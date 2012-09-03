06adad420abf1e81200974a7be9c80a97bd2ba7f
machado.rodrigom <machado.rodrigom@0c8ed97b-17ea-a244-a63b-4f1c4785ee99>
2012-09-03 20:28:14 +0000

    Alterações

--- trunk/src/br/com/webcopias/controller/Login.java
+++ trunk/src/br/com/webcopias/controller/Login.java
@@ -2,11 +2,11 @@ package br.com.webcopias.controller;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 
 @ManagedBean(name = "login")
-@SessionScoped
+@RequestScoped
 public class Login {
 	private String user,password;
 
@@ -16,6 +16,7 @@ public class Login {
 
 	public void setUser(String user) {
 		this.user = user;
+		System.out.println("uooooooooooooooooooo");
 	}
 
 	public String getPassword() {
@@ -24,14 +25,18 @@ public class Login {
 
 	public void setPassword(String password) {
 		this.password = password;
+		System.out.println("uaaaaaaaaaaaaaaaaa");
 	}
 	
 	public void checkLogin(){
 		if(user == null){
-			FacesMessage msg = new FacesMessage("Erro. ", "Usu·rio n„o informado");
+			FacesMessage msg = new FacesMessage("Erro. ", "Usu√°rio n√£o informado");
 			FacesContext.getCurrentInstance().addMessage(null, msg);
 		}else if(password == null){
-			FacesMessage msg = new FacesMessage("Erro. ", "Usu·rio n„o informado");
+			FacesMessage msg = new FacesMessage("Erro. ", "Senha n√£o informado");
+			FacesContext.getCurrentInstance().addMessage(null, msg);
+		}else{
+			FacesMessage msg = new FacesMessage("Erro. ", "Usu√°rio ou senha incorreto");
 			FacesContext.getCurrentInstance().addMessage(null, msg);
 		}
 	}
